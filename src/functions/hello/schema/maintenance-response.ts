bb1b83e594f5e58524efa5b72feb7120f0c7d898
snozaki <snozaki@netprotections.co.jp>
2022-04-29 16:23:57 +0900

    MOD: defaultの廃止

--- src/functions/hello/schema/maintenance-response.ts
+++ src/functions/hello/schema/maintenance-response.ts
@@ -1,4 +1,7 @@
-export default {
+/**
+ * メンテナンスレスポンススキーマ
+ */
+export const helloMaintenanceReponseSchema = {
   $schema: "http://json-schema.org/draft-04/schema#",
   title: "HelloResponse",
   type: "object",
@@ -7,4 +10,20 @@ export default {
     to: { type: "string" },
     message: { type: "string" },
   }
+}
+
+const baseDatetime = new Date();
+const toDatetime = baseDatetime;
+toDatetime.setHours(baseDatetime.getHours() + 3);
+
+/**
+ * メンテナンスレスポンス
+ */
+export const maintenanceResponse = {
+  from: baseDatetime.toISOString(),
+  to: toDatetime.toISOString(),
+  message: `
+  システムアップデートのためサービスを停止しています。
+  お客様にはご不便をおかけしますが、メンテナンス終了まで今しばらくお待ちください。
+  `,
 }
